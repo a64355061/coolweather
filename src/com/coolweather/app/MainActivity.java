e393ecadd5ccc7fedaae644e2543d077115d1183
kidd <459041524@qq.com>
2014-12-29 15:40:19 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/com/coolweather/app/MainActivity.java
@@ -0,0 +1,22 @@
+package com.coolweather.app;
+
+import com.example.coolweather.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    
+   
+}
